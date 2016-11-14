229dd6af13eb774017aa36f33f8f50083bca2c49
Peter Wille <pewi@dr.dk>
2016-11-15 00:19:42 +0100

    added Fire and Scan

---
+++ AIGame/CoreGame/Orders/Fire.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AIGame.CoreGame.Orders
+{
+    public class Fire : IOrder
+    {
+        public OrderType Type()
+        {
+            return OrderType.Scan;
+        }
+
+
+        public void Execute(Unit unit, Map map)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsValid(Unit unit, Map map)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+}
+
---
+++ AIGame/CoreGame/Orders/Scan.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace AIGame.CoreGame.Orders
+{
+    public class Scan : IOrder
+    {
+        public OrderType Type()
+        {
+            return OrderType.Scan;
+        }
+
+
+        public void Execute(Unit unit, Map map)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsValid(Unit unit, Map map)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+}
+
+
+
+    }
+}
